1644a8367f3aa79684b23fcf197d7a22559d04ab
muttu <muttubk140601@gmail.com>
2023-12-15 20:06:22 +0530

    changed login route from get to post method

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 3000
 
--- routes/user.js
+++ routes/user.js
@@ -40,7 +40,7 @@ router.post('/register', async (req, res) => {
     }
 })
 
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const { email, password } = req.body
         const user = await User.findOne({ email })
